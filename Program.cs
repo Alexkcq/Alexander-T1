934a6fbeaf0ae002fe3babeb7b44adc0187d1ca6
Alexkcq <qalexander785@gmail.com>
2024-09-19 08:58:54 -0500

    P1

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿double sueldo, descuento;
+Console.WriteLine("ingrese su sueldo bruto: ");
+sueldo=double.Parse(Console.ReadLine());
+while (sueldo <= 600)
+{
+    descuento=sueldo * 0.05;
+    NullReferenceException 
+}
